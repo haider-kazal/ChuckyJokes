1868bbb971ef267b9fa9ff3d3cb2098476e050fd
Haider Ali Kazal <haider_kazal@hotmail.com>
2020-07-12 00:27:18 +0600

    Initial Commit

---
+++ ChuckyJokes/ChuckyJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckyJokesApp.swift
+//  ChuckyJokes
+//
+//  Created by Haider Ali Kazal on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct ChuckyJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
